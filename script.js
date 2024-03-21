411ae1bde955ebab6af998390356d6ef55c7bd1f
Yui Jensen <kyanyui67@gmail.com>
2024-03-22 00:25:03 +0100

    Upload HTML, Sass and JavaScript
    Upload gallery and contact section

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ const animationItems = document.querySelectorAll(".animate");
 animationItems.forEach((item) => {
   observer.observe(item);
 });
+
+function sendEmail() {
+  Email.send({
+    SecureToken: "8e4b2ae7-bfa9-417d-be33-ec974d827a1e",
+    To: "kyanyui67@gmail.com",
+    From: document.getElementById("email").value,
+    Subject: "New Contact Form Enquiry",
+    Body:
+      "Name: " +
+      document.getElementById("name").value +
+      "<br> Email: " +
+      document.getElementById("email").value +
+      "<br>Phone Number: " +
+      document.getElementById("phone").value +
+      "<br>Meseage: " +
+      document.getElementById("message").value,
+  }).then((message) => {
+    alert("Email sent successfully!");
+  });
+}
